9e97c57c55531ba6b61bda61ea948fb78e67e3fc
neil patel <neil@myindianweddingapp.com>
2017-03-09 16:36:30 +0000

    Implemented chefs edit features and test

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -4,6 +4,20 @@ class ChefsController < ApplicationController
     @chef = Chef.new
   end
 
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to @chef
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @chef = Chef.new(chef_params)
     if @chef.save
@@ -15,6 +29,7 @@ class ChefsController < ApplicationController
   end
 
   def show
+    @chef = Chef.find(params[:id])
   end
 
   private
