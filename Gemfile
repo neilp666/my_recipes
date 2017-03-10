23abcb4d35bfc0bcfdb824fa27d95620dc6b174d
neil patel <neil@myindianweddingapp.com>
2017-03-10 19:33:04 +0000

    Added pagination to app

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'will_paginate', '3.1.0'
+gem 'bootstrap-will_paginate', '0.0.10'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -5,7 +5,7 @@ class ChefsController < ApplicationController
   end
 
   def index
-    @chefs = Chef.all
+    @chefs = Chef.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
@@ -34,6 +34,7 @@ class ChefsController < ApplicationController
 
   def show
     @chef = Chef.find(params[:id])
+    @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
   end
 
   private
