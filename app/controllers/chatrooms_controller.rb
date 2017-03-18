d7a7a41a4bc733bc1a3b8f4d41b329943e860dc3
neil patel <neil@myindianweddingapp.com>
2017-03-18 11:35:40 +0000

    Complete Chef chat feature

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,9 @@
 class ChatroomsController < ApplicationController
+  before_action :require_user
 
   def show
     @message = Message.new
-    @messages = Message.all
+    @messages = Message.most_recent
   end
 
 end
