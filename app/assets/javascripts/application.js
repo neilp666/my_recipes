bdd91656614dc1447a14cb3f6676cffe5e048166
neil patel <neil@myindianweddingapp.com>
2017-03-18 11:55:42 +0000

    fixed typo on js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 //= require_tree .
 
-ffunction scrollToBottom(){
+function scrollToBottom(){
   if($('#messages').length > 0) {
     $('#messages').scrollTop($('#messages')[0].scrollHeight);
   }
