942680c93130505db8a581b4a169035094ae604b
neil patel <neil@myindianweddingapp.com>
2017-03-20 15:17:23 +0000

    Add production image upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [250,250]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
